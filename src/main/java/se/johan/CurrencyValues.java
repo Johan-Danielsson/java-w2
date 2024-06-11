7af2918efc6e5bf4505d3234d681fb9e3dd401cf
Johan-Danielsson <johan.dunhill@gmail.com>
2024-06-11 11:30:01 +0200

    Added CurrencyValues class file with exchange rates

---
+++ src/main/java/se/johan/CurrencyValues.java
@@ -0,0 +1,17 @@
+package se.johan;
+
+public class CurrencyValues {
+
+    // Conversion values from xe.com for SEK - USD / EUR
+
+    // SEK-USD
+    static double sekUsd = 0.09533724;
+    static double usdSek = 10.4891;
+
+    // EUR-SEK
+    static double sekEur = 0.088583264;
+    static double eurSek = 11.2897;
+
+
+
+}
