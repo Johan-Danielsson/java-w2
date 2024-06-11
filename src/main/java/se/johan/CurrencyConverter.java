a648095ac529a792bb058c57804cda2bc5fd9211
Johan-Danielsson <johan.dunhill@gmail.com>
2024-06-11 12:15:03 +0200

    Added try and catch InputMismatchException to handle non-numerical inputs. Also fixed some formatting of text.

--- src/main/java/se/johan/CurrencyConverter.java
+++ src/main/java/se/johan/CurrencyConverter.java
@@ -1,5 +1,5 @@
 package se.johan;
-
+import java.util.InputMismatchException;
 import java.time.temporal.ChronoUnit;
 import java.util.Scanner;
 
@@ -12,6 +12,7 @@ public class CurrencyConverter {
         // while loop to run the app until exit
         while (true) {
             System.out.println("Currency Converter App");
+            System.out.println("----------------------");
             System.out.println("1. Convert SEK to USD");
             System.out.println("2. Convert USD to SEK");
             System.out.println("3. Convert SEK to EUR");
@@ -19,7 +20,17 @@ public class CurrencyConverter {
             System.out.println("5. Exit the converter");
             System.out.println("Enter your choice:");
 
-            int option = input.nextInt();
+            int option = 0;
+
+            // Added try and catch InputMismatchException to handle non numerical inputs
+            try {
+
+                option = input.nextInt();
+            } catch (InputMismatchException e) {
+                System.out.println("----------------------------------");
+                System.out.println("Only numerical inputs are allowed.");
+                input.next();
+            }
 
             // Sending input to correct result
             if (option == 1) {
@@ -28,25 +39,30 @@ public class CurrencyConverter {
                 Calculations.convertSekUsd(num1, CurrencyValues.sekUsd);
             }
 
-            if (option == 2) {
+            else if (option == 2) {
                 System.out.println("Enter amount of USD:");
                 double num1 = input.nextDouble();
                 Calculations.convertUsdSek(num1, CurrencyValues.usdSek);
             }
-            if (option == 3) {
+            else if (option == 3) {
                 System.out.println("Enter amount of SEK:");
                 double num1 = input.nextDouble();
                 Calculations.convertSekEur(num1, CurrencyValues.sekEur);
             }
-            if (option == 4) {
+            else if (option == 4) {
                 System.out.println("Enter amount of EUR:");
                 double num1 = input.nextDouble();
                 Calculations.convertEurSek(num1, CurrencyValues.eurSek);
             }
-            if (option == 5) {
+            else if (option == 5) {
                 System.out.println("Thank you for using the converter.");
                 System.out.println("Exiting at " +Calculations.currentTime.truncatedTo(ChronoUnit.SECONDS));
                 System.exit(0);
+            }
+            else {
+                System.out.println("----------------------------------");
+                System.out.println("Please enter a number from 1 to 5.");
+                System.out.println("----------------------------------");
             }
 
         }
